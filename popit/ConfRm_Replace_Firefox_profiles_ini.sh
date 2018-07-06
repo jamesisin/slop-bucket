4b15990b272b64ed8ee186c47df7eb804453f8b4
JamesIsIn <jamesisin@gmail.com>
2018-07-06 06:10:51 -0700

    Update ConfRm_Replace_Firefox_profiles_ini.sh

--- popit/ConfRm_Replace_Firefox_profiles_ini.sh
+++ popit/ConfRm_Replace_Firefox_profiles_ini.sh
@@ -1,23 +1,22 @@
-#! /bin/bash
-## Fix Firefox profile manager error on machines with re-directed home directories.
-## by JamesIsIn
+#! /usr/bin/env bash 
+# Fix Firefox profile manager error on machines with re-directed home directories. 
+# by JamesIsIn 
+# Do something nice today. 
+## 
 
 
-#Get current logged-in username.
-
-username=$(stat -f %Su /dev/console)
-
-
-# Get first profile name in user's Library folder.
-
-profile="$(basename /home/"$username"/Library/Application\ Support/Firefox/Profiles/* | head -1)"
+#Get current logged-in username. 
+username=$( stat -f %Su /dev/console ) 
 
 
-# Empty and populate user's Firefox profiles file.
-
-printf "[General]\nStartWithLastProfile=1\n\n[Profile0]\nName=Default User\nIsRelative=1\n" 1>/home/"$username"/Library/Application\ Support/Firefox/profiles.ini
-printf "Path=Profiles/""$profile" 1>>/home/"$username"/Library/Application\ Support/Firefox/profiles.ini
+# Get first profile name in user's Library folder. 
+profile="$( basename /home/"${username}"/Library/Application\ Support/Firefox/Profiles/* | head -1 )" 
 
 
-exit
-##
+# Empty and populate user's Firefox profiles file. 
+printf "[General]\nStartWithLastProfile=1\n\n[Profile0]\nName=Default User\nIsRelative=1\n" 1>/home/"${username}"/Library/Application\ Support/Firefox/profiles.ini 
+printf "Path=Profiles/""${profile}" 1>>/home/"${username}"/Library/Application\ Support/Firefox/profiles.ini 
+
+
+exit 0 
+## 
